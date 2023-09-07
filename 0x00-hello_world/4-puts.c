de8a7ec205c0021190aa9558ba2627b88ee00244
hossam9633 <hamedhossam2013@gmail.com>
2023-09-07 00:43:08 -0700

    ch

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * Description: A C Programm that print with put function
  *
  *Return: alawys 0 (success)
- * /
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
