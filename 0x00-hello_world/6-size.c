0b268412e865fd45c4c5374d51ddab12a5808255
hossam9633 <hamedhossam2013@gmail.com>
2023-09-07 00:56:39 -0700

    ch

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C Programm that print with put function
+ *
+ *Return: alawys 0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of a char:",sizeof(char));
+	printf("Size of a int:",sizeof(int));
+	printf("Size of a long int:",sizeof(long int));
+	printf("Size of a long long int:",sizeof(long long int));
+	printf("Size of a float:",sizeof(float));
+	return (0);
+}
