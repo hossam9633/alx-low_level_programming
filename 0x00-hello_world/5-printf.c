56496e598a070213dd961b2f1d60e26ce2958060
hossam9633 <hamedhossam2013@gmail.com>
2023-09-07 00:47:23 -0700

    ch

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C Programm that print with put function
+ *
+ *Return: alawys 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
