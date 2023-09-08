fd9652cb7ca3afc30f4e11002f2dd8c2e2556233
hossam9633 <hamedhossam2013@gmail.com>
2023-09-08 12:34:31 -0700

    ch

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,14 +15,15 @@ int main(void)
 
 	for (a = 0; a <= 8; a++)
 	{
-	for (b = 1; b <= 9; b++)
+	for (b = a + 1; b <= 9; b++)
 	{
 	putchar('0' + a);
 	putchar('0' + b);
-	if (a == 8 && b == 9)
-	break;
+	if (a != 8 || b != 9)
+	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	putchar('\n');
