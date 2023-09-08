df45b889be9f862b76126bf7ded92c3414ab4704
hossam9633 <hamedhossam2013@gmail.com>
2023-09-08 10:21:09 -0700

    ch

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: for loop an inside it putchar to print alphapet
+*
+* Return: alawys 0 (success)
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+	putchar('0' + a);
+	putchar(',');
+	putchar(' ');
+	}
+	return (0);
+}
