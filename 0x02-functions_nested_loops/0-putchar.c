baf9d41fda75fee8d44dae4d68fe44b5ef783b30
hossam9633 <hamedhossam2013@gmail.com>
2023-09-12 12:58:38 -0700

    ch

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: for loop  putchar to print lowercase and upper  alphapet
+*
+* Return: alawys 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
