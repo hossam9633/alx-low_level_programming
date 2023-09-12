d552bd23651509da31f1877aee01d9653d995615
hossam9633 <hamedhossam2013@gmail.com>
2023-09-12 14:19:05 -0700

    ch

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - entry point
+*
+* Description: for loop  putchar to print lowercase and upper  alphapet
+*
+* Return: alawys 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 97; ch < 123; ch++)
+	{
+	_putchar('ch');
+	}
+	_putchar('\n');
+}
