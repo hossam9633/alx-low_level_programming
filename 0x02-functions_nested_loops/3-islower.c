e2fa1c05c4c275cfac9adc0c8fed94ba32f49b2b
hossam9633 <hamedhossam2013@gmail.com>
2023-09-12 15:13:34 -0700

    ch

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,9 +4,10 @@
 *
 * _islower - function to check if
 *	character is lowercase
+*
 * @c: checks input of function
 *
-* Return: returns 1 if 'c is lowercase
+* Return: returns 1 if 'c' is lowercase
 *	otherwise always 0 (Success)
 */
 
